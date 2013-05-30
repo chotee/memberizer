c33c498c7945c001cda180373c5f15d3b57367a8
Chotee <chotee@openended.eu>
2013-05-31 00:39:46 +0200

    Initial fake LDAP works.

--- members2accounts/config.py
+++ members2accounts/config.py
@@ -8,10 +8,10 @@ def Config():
 class _Config(dict):
     def __init__(self):
         self.update({
-            'server_uri': 'ldap://192.168.122.224',
+            'server_uri': None, #'ldap://192.168.122.224',
             'base_dn': 'dc=techinc,dc=nl',
-            'admin_user': 'cn=admin,dc=techinc,dc=nl',
-            'admin_pass': 'SomeThingSecret',
+            'admin_user': '', #'cn=admin,dc=techinc,dc=nl',
+            'admin_pass': '', #'SomeThingSecret',
             'people_dn': 'ou=people,dc=techinc,dc=nl',
             'groups_dn': 'ou=groups,dc=techinc,dc=nl',
             'member_group': 'members',
