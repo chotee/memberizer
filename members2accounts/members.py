261b1010a468da603efa65cb0b3dbc35a6f632b9
Chotee <chotee@openended.eu>
2013-06-01 10:29:18 +0200

    Loading json object from file.

--- members2accounts/members.py
+++ members2accounts/members.py
@@ -1,7 +1,21 @@
 
+import json
+from members2accounts.config import Config
+
 class Members(object):
+    def __init__(self):
+        self.member_data = None
     def verify_validity(self):
         pass
+
+    def list_members(self):
+        if self.member_data is None:
+            self.member_data = self.load_member_data()
+        return self.member_data
+
+    def load_member_data(self):
+        json_stream = open(Config()['members.json'])
+        return json.load(json_stream)
 
 class Member(object):
     def __init__(self, email=None):
--- members2accounts/test/test_members.py
+++ members2accounts/test/test_members.py
@@ -1,14 +1,24 @@
 __author__ = 'chotee'
 
 import py
+import pytest
 
 from members2accounts.members import Members
 from members2accounts.config import Config
+
+@pytest.fixture
+def test_json(request):
+    Config()['members.json'] = unicode(py.path.local(__file__).join('../test_members.json').realpath())
 
 class TestMembers(object):
     def test_init(self):
         Members()
 
-    def test_list_members(self, monkeypatch):
-        Config()['members.json'] = py.path.local(__file__).join('../test_members.json').realpath()
-        assert Config()['members.json'] == 'foo'
+    def test_load_member_data(self, test_json):
+        assert Config()['members.json'] == py.path.local(__file__).join('../test_members.json').realpath()
+        m = Members()
+        assert 3 == len(m.load_member_data())
+
+    def test_load_member_data_missing_file(self):
+        Config()['members.json'] = 'doesnotexist.'
+        pytest.raises(IOError, Members().load_member_data)
