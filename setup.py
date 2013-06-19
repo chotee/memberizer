18e2cba570f32a3d3b299617272c13150bb12211
Chotee <chotee@openended.eu>
2013-06-20 00:00:43 +0200

    Initial setup stub

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='members2accounts',
+      version='1.0',
+      description='Take a member file and create LDAP accounts',
+      author='Chotee',
+      author_email='chotee@openended.eu',
+      url='https://www.github.com/chotee/members2accounts',
+      packages=['members2accounts'],
+     )
